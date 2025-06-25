43ac15b128a46f6e8c721be5e90c2168b2c34fbb
Saba Chitorelidze <chitorelidze.99@gmail.com>
2025-06-25 14:57:28 +0400

    change link

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   publicDir: "public", // ეს ნიშნავს რომ public/_redirects მოხვდება dist-ში
   server: {
     proxy: {
-      "/api": "https://serene-waters-93778-60a65ffb64cd.herokuapp.com",
-      "/uploads": "https://serene-waters-93778-60a65ffb64cd.herokuapp.com",
+      "/api": "https://stark-peak-56683-4ee4ea1ccf4b.herokuapp.com/",
+      "/uploads": "https://stark-peak-56683-4ee4ea1ccf4b.herokuapp.com/",
     },
   },
 });
